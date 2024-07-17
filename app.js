61c0decc296fff3d9c4942370effd9b668e51324
nipatt <nipat679@gmail.com>
2024-07-17 14:29:30 +0700

    Update app.js world to pond

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const debug = require("debug")("app");
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello pond!'))
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
