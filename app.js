b28a8f2df01b7667aa1c8e6e70502d3ccff4b981
nipatt <nipat679@gmail.com>
2024-07-17 14:47:59 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
-const chalk = require("chalk")
 const debug = require("debug")("app");
+const path = require("path");
+
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello pond!'))
+app.use(express.static(path.join(__dirname,"/public/")))
 
+app.get('/', (req, res) => {
+    res.send('Hello pond!')
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
